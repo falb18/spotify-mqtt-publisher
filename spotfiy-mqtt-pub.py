c0c9bbfe0704ee3eba35f25224068dbc326ab1c1
falb18 <arturo.lopezbo@gmail.com>
2024-03-25 19:27:33 -0600

    Add mqtt topics to publish the spotify's metadata

--- spotfiy-mqtt-pub.py
+++ spotfiy-mqtt-pub.py
@@ -4,26 +4,41 @@ from paho.mqtt import client as mqtt_client
 from mpris2.mpris2 import player, interfaces
 from mpris2.mpris2.types import Metadata_Map
 
-# MPRIS2 helper functions and variables
+#------------------------------------------------------------------------------
+# MPRIS2 helper functions and variables:
 #------------------------------------------------------------------------------
 
 spotify_bus_name = f"{interfaces.Interfaces.MEDIA_PLAYER}.spotify"
 spotify_player = player.Player(dbus_interface_info={'dbus_uri': spotify_bus_name})
 
-def get_metadata_from_player() -> None:
+def get_metadata_from_player() -> list:
+    metadata = list()
     if spotify_player.PlaybackStatus == "Playing" or spotify_player.PlaybackStatus == "Paused":
-        print("Current track information:")
-        print(f"  Title:  {spotify_player.Metadata[Metadata_Map.TITLE]}")
-        print(f"  Album:  {spotify_player.Metadata[Metadata_Map.ALBUM]}")
+        metadata.append(spotify_player.Metadata[Metadata_Map.TITLE])
+        metadata.append(spotify_player.Metadata[Metadata_Map.ALBUM])
         # Dbus Array of strings, we need the first element for the artist's name
-        print(f"  Artist: {spotify_player.Metadata[Metadata_Map.ARTIST][0]}")
+        metadata.append(spotify_player.Metadata[Metadata_Map.ARTIST][0])
+    
+    return metadata
 
-    None
+
+#------------------------------------------------------------------------------
+# Spotify MQTT publisher:
+#------------------------------------------------------------------------------
 
 mqtt_broker = "localhost"
 mqtt_port = 1883
 mqtt_client_id = "spotify-pub"
-topic = "spotify/"
+
+# Spotify's MQTT topics
+main_topic = "spotify"
+metadata_topic = f"{main_topic}/metadata"
+# track_title is a string type
+track_title_topic = f"{metadata_topic}/track_title"
+# album_name is a string type
+album_name_topic = f"{metadata_topic}/album_name"
+# track_artist is a string type
+track_artist_topic = f"{metadata_topic}/track_artist"
 
 # Callback functions for mqtt client
 #------------------------------------------------------------------------------
@@ -37,6 +52,7 @@ def on_connect_cbk(client, userdata, flags, reason_code, properties):
 def on_disconnect_cbk(client, userdata, flags, reason_code, properties):
     print(f"Disconnected from {client.host} with reason code: {reason_code}.")
 
+# Functions:
 #------------------------------------------------------------------------------
 
 def connect_mqtt():
@@ -56,6 +72,9 @@ def parse_args():
     parser.add_argument('--port', type=int, default=mqtt_port)
     return parser.parse_args()
 
+# Main loop:
+#------------------------------------------------------------------------------
+
 def run_publisher():
     cmd_args = parse_args()
     mqtt_client = cmd_args.broker
@@ -65,14 +84,14 @@ def run_publisher():
     print("To exit press Ctrl+c\n")
 
     try:
-        count = 0
         while True:
-            get_metadata_from_player()
-            message = f"Test message {count}"
-            client.publish(topic, message)
-            print(f"Published: {message}")
+            spotify_metadata = get_metadata_from_player()
+
+            client.publish(track_title_topic, spotify_metadata[0])
+            client.publish(album_name_topic, spotify_metadata[1])
+            client.publish(track_artist_topic, spotify_metadata[2])
+
             time.sleep(5)
-            count += 1
     
     except KeyboardInterrupt:
         print("Exiting...")
